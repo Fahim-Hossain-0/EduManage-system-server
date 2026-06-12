ca8f9c90316528f26ffb85774e7ceccc82579385
fahim-hossain-0 <fahimhossain735@gmail.com>
2026-06-13 02:57:32 +0600

    fix vercel deployment

--- src/app.js
+++ src/app.js
@@ -1,40 +1,31 @@
-const express = require('express');
-const cors = require('cors');
-
-
-
-const userRoutes = require('./routes/user.routes');
-const addNewClassRoutes = require('./routes/addNewClass.routes');
-const teacherRoutes = require('./routes/teacher.routes');
-const paymentRoutes = require('./routes/payment.routes')
-const assignmentRoutes = require("./routes/assignment.route");
-const submissionRoutes = require("./routes/submission.route");
-const evaluationRoutes = require("./routes/evaluation.route");
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
 const express = require("express");
 const cors = require("cors");
 
+const userRoutes = require("./routes/user.routes");
+const addNewClassRoutes = require("./routes/addNewClass.routes");
+const teacherRoutes = require("./routes/teacher.routes");
+const paymentRoutes = require("./routes/payment.routes");
+const assignmentRoutes = require("./routes/assignment.route");
+const submissionRoutes = require("./routes/submission.route");
+const evaluationRoutes = require("./routes/evaluation.route");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Test Route
+// Root Route
 app.get("/", (req, res) => {
-  res.send("EduManage Server Running 🚀");
+  res.send("EduManage API Running 🚀");
 });
 
-app.use('/', userRoutes);
-app.use('/', addNewClassRoutes);
-app.use('/', teacherRoutes);
-app.use('/',paymentRoutes);
+// Routes
+app.use("/", userRoutes);
+app.use("/", addNewClassRoutes);
+app.use("/", teacherRoutes);
+app.use("/", paymentRoutes);
 app.use("/", assignmentRoutes);
 app.use("/", submissionRoutes);
-app.use("/",evaluationRoutes);
-
+app.use("/", evaluationRoutes);
 
 module.exports = app;
