781da361826a18f74d43a1e4ba7790421d2657b4
fahim-hossain-0 <fahimhossain735@gmail.com>
2026-06-14 01:51:39 +0600

    fix vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,23 +1,16 @@
-require("dotenv").config();
-
 const serverless = require("serverless-http");
 
 const app = require("../src/app");
 const { connectDB } = require("../src/config/db");
 
-let initialized = false;
-
-async function init() {
-  if (!initialized) {
-    await connectDB();
-    initialized = true;
-  }
-}
-
-const handler = serverless(app);
+let handler;
 
 module.exports = async (req, res) => {
-  await init();
+  await connectDB();
+
+  if (!handler) {
+    handler = serverless(app);
+  }
 
   return handler(req, res);
 };
