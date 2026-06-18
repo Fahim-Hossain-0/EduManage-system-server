5273c8efb0a32a5a69e15542db536e7da31e8b9c
fahim-hossain-0 <fahimhossain735@gmail.com>
2026-06-18 15:30:17 +0600

    fix vercel deployment

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,3 +1,8 @@
+require("dotenv").config();
+
+console.log("FB_SERVICE_KEY exists:", !!process.env.FB_SERVICE_KEY);
+console.log("Length:", process.env.FB_SERVICE_KEY?.length);
+
 const { initializeApp, cert, getApps } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
 
@@ -5,7 +10,6 @@ const decoded = Buffer.from(
   process.env.FB_SERVICE_KEY,
   "base64"
 ).toString("utf8");
-
 const serviceAccount = JSON.parse(decoded);
 
 if (!getApps().length) {
