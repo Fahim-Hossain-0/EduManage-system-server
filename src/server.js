85138c5df1c2c6c339ff8a81ea30ac62b77737b0
fahim-hossain-0 <fahimhossain735@gmail.com>
2026-06-18 15:04:55 +0600

    remove api/index,fix server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const app = require("./app");
-const { connectDB } = require("./config/db");
+const app = require('./app');
+const { connectDB } = require('./config/db');
+
+connectDB();  
+
+
+
+
 
 const PORT = process.env.PORT || 5000;
 
-async function startServer() {
-  await connectDB();
-
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
-startServer();
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
