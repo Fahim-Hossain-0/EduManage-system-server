45f531259e3b6ea0ea93684660f172a8357210fe
fahim-hossain-0 <fahimhossain735@gmail.com>
2026-06-23 12:04:39 +0600

    update

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,21 +1,21 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-// console.log("FB_SERVICE_KEY exists:", !!process.env.FB_SERVICE_KEY);
-// console.log("Length:", process.env.FB_SERVICE_KEY?.length);
+// // console.log("FB_SERVICE_KEY exists:", !!process.env.FB_SERVICE_KEY);
+// // console.log("Length:", process.env.FB_SERVICE_KEY?.length);
 
-const { initializeApp, cert, getApps } = require("firebase-admin/app");
-const { getAuth } = require("firebase-admin/auth");
+// const { initializeApp, cert, getApps } = require("firebase-admin/app");
+// const { getAuth } = require("firebase-admin/auth");
 
-const decoded = Buffer.from(
-  process.env.FB_SERVICE_KEY,
-  "base64"
-).toString("utf8");
-const serviceAccount = JSON.parse(decoded);
+// const decoded = Buffer.from(
+//   process.env.FB_SERVICE_KEY,
+//   "base64"
+// ).toString("utf8");
+// const serviceAccount = JSON.parse(decoded);
 
-if (!getApps().length) {
-  initializeApp({
-    credential: cert(serviceAccount),
-  });
-}
+// if (!getApps().length) {
+//   initializeApp({
+//     credential: cert(serviceAccount),
+//   });
+// }
 
-module.exports = getAuth();
+// module.exports = getAuth();
